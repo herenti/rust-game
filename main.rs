c1b58faa717e44fce0ddde540144cb50534afdad
herenti <106109941+herenti@users.noreply.github.com>
2025-08-14 18:12:27 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -8,11 +8,15 @@ use colored::Colorize;
 use std::io;
 use std::io::Write;
 use std::collections::HashMap;
+use std::fs::File;
+
 
 fn save(data: &mut User) {
-    let mut file = File::open("utils.txt");
-    let items = "-".join(data.items);
-    file.write_all(format!("{}:{}:{}:{}", data.name, items, data.progress, data.route));
+    let mut file = File::create("utils.txt");
+    let items = data.items.join("-");
+    let info = format!("{}:{}:{}:{}", data.name, items, data.progress, data.route);
+    let info = info.as_bytes();
+    file.expect("reason").write_all(info);
 }
 
 struct Story{
@@ -41,7 +45,7 @@ impl Story{
         //       //
         ///////////
 
-        story.start.insert(0000, format!("It was a cloudy and brisk day in the small village where my story began. The story of finding the great treasure of my {}.".to_string(), "heart".red().bold()));
+        story.start.insert(1, format!("It was a cloudy and brisk day in the small village where my story began. The story of finding the great treasure of my {}.", "heart".red().bold()));
         story
     }
 }
@@ -50,10 +54,21 @@ struct Help {
 }
 
 impl Help {
-    fn lookup(term: &str) -> String {
+    fn lookup(term: &str, user: User) -> String {
         let resp = match term {
-            "list" => "[save, help, load, quit, lastmessage, new]".to_string(),
-            _ => format!("{}", "INVALID HELP TERM".red().bold()),
+            "list" => {
+                "[save, help, load, quit, lastmessage, new]".to_string()
+            }
+            "me" => {
+                if user.progress > 0 {
+                    format!("\r\nName: {}\r\nItems: {:?}", user.name.yellow(), user.items)
+                } else {
+                    format!("You have not started a game yet. Use the [{}] new command.", "new".green())
+                }
+            }
+            _ => {
+                format!("{}", "INVALID HELP TERM".red().bold())
+            }
         };
         resp
     }
@@ -67,7 +82,6 @@ struct Commands {
 
 impl Commands {
     fn new(command: &str, story: &mut Story) {
-        let mut
         let args = command.split(" ");
         let args: Vec<&str> = args.collect();
         let command = args[0];
@@ -78,13 +92,14 @@ impl Commands {
         };
         let command = command.to_lowercase();
         let command = command.as_str();
-        let start_event_mark = 0050;
+        let start_event_mark = 50;
+        let mut user = User::new("");
         match command {
             "help" => {
                 let resp = if args.len() == 0 {
                     format!("How to use the help command. You can look up a characters name, or an item name like so: [{}]. [{}]. To show a list of commands use [{}]. Press enter with no command to continue the story.", "help itemnamehere".green(), "help commandnamehere".green(), "help list".green())
                     } else {
-                        Help::lookup(&args)
+                        Help::lookup(&args, user)
                     };
                 println!("[{}]: {}", "Help".blue().bold(), &resp);
 
@@ -93,11 +108,34 @@ impl Commands {
                     //load from file, play last progress message
                 }
                 "new" => {
-                    let mut user = User::new();
+                    print!("[{}]: ", "Enter name".blue().bold());
+                    std::io::stdout().flush().unwrap();
+                    let mut input = String::new();
+                    io::stdin().read_line(&mut input).expect("Failed to read input.");
+                    let input = input.trim();
+                    let mut user = User::new(&input);
+                    user.progress = 1;
+                    save(&mut user);
+                    println!("created new user: {}", input);
                 }
                 "" => {
-                    println!("storyprogress");
-                    //continue story, if no user then prompt to new game. if user progress num == event mark, start event loop.
+                    if user.progress > 0 {
+                        let route = &user.route;
+                        match route.as_str() {
+                            "start" => {
+                                println!("{}", story.start[&user.progress]);
+                            }
+
+                        _ => {
+                            println!("you have not started the game yet. use the new command.");
+                        }
+
+                        //continue story, if no user then prompt to new game. if user progress num == event mark, start event loop.
+                    }
+                }
+                else {
+                    println!("you have not started the game yet. use the new command.");
+                }
                 }
 
             _ => {
@@ -124,36 +162,34 @@ impl User {
         if let Ok(contents) = std::fs::read_to_string("utils.txt"){
             let contents = contents.trim().split(":");
             let contents = contents.collect::<Vec<&str>>();
-            if contents.len() < 1 {
-            let mut user = User {
+            if contents.len() < 4 {
+            User {
                 name: name.to_string(),
                 items: vec![],
                 progress: 0,
                 route: "start".to_string(),
-            };
-            user
+            }
             } else {
                 let name = contents[0];
                 let items = contents[1];
                 let items = items.split("-");
-                let items = items.collect::<Vec<String>>();
-                let mut user = User {
+                let items: Vec<String> = items.map(|x| x.trim().to_string()).collect();
+                User {
                     name: name.to_string(),
                     items: items,
                     progress: contents[2].parse().expect("reason"),
                     route: contents[3].to_string(),
-                };
-                user
-            };
+                }
+
+            }
         } else {
             let _file = File::create("utils.txt");
-            let mut user = User {
+            User {
                 name: name.to_string(),
                 items: vec![],
                 progress: 0,
                 route: "start".to_string(),
-            };
-            user
+            }
         }
     }
 }
